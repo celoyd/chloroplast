c6c94e5a5e16dd2921989a3c7e485983f974efcc
Paul Mison <misonp@gmail.com>
2018-12-06 15:34:08 -0800

    Match arguments

--- list-prefix.py
+++ list-prefix.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
 
-import os, sys
+import argparse
+import os
+import sys
 
 from datetime import datetime
 
 import boto3
 
-frame = 'F'
-band = 1
+parser = argparse.ArgumentParser(description='Check s3 for file')
+parser.add_argument('frame', help='Frame type (F, C, M)')
+parser.add_argument('band', help='Band (1, 2, 3)')
+parser.add_argument('year', help='Year')
+parser.add_argument('doy', help='Day of year (UTC)')
+parser.add_argument('hour', help='Hour (UTC)')
+parser.add_argument('minute', help='Minute')
 
-now = datetime.utcnow()
-year = now.year
-doy = now.timetuple().tm_yday
-hour = now.hour
-minute = 0
+args = parser.parse_args()
 
-directory = f"ABI-L1b-Rad{frame}/{year}/{doy}/{hour}/"
-filename = f"OR_ABI-L1b-Rad{frame}-M3C0{band}_G17_s{year}{doy}{hour}{minute}"
+directory = f"ABI-L1b-Rad{args.frame}/{args.year}/{args.doy}/{args.hour}/"
+filename = f"OR_ABI-L1b-Rad{args.frame}-M3C0{args.band}_G17_s{args.year}{args.doy}{args.hour}{args.minute}"
 prefix = directory+filename
 
 print(f"Checking for {prefix}")
